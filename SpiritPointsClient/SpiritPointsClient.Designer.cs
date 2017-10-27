d9552b9cd42a9c1ced3652fd8606cd974d8a74d8
BuildTools <unconfigured@null.spigotmc.org>
2017-10-26 22:09:06 -0700

    Added extremely basic form design

---
+++ SpiritPointsClient/SpiritPointsClient.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SpiritPointsClient
+{
+    partial class SpiritPointsClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.Label();
+            this.Grade = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(434, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(568, 516);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(12, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(348, 39);
+            this.Title.TabIndex = 1;
+            this.Title.Text = "Spirit Points Manager";
+            this.Title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(15, 79);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(71, 24);
+            this.Name.TabIndex = 2;
+            this.Name.Text = "Name: ";
+            // 
+            // Grade
+            // 
+            this.Grade.AutoSize = true;
+            this.Grade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Grade.Location = new System.Drawing.Point(15, 124);
+            this.Grade.Name = "Grade";
+            this.Grade.Size = new System.Drawing.Size(67, 24);
+            this.Grade.TabIndex = 3;
+            this.Grade.Text = "Grade:";
+            // 
+            // SpiritPointsClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1014, 540);
+            this.Controls.Add(this.Grade);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SpiritPointsClient";
+            this.Text = "Spirit Point Client";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label Grade;
+    }
+}
+
--- SpiritPointsClient/SpiritPointsClient.cs
+++ SpiritPointsClient/SpiritPointsClient.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace SpiritPointsClient
 {
-    public partial class Form1 : Form
+    public partial class SpiritPointsClient : Form
     {
-        public Form1()
+        public SpiritPointsClient()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
